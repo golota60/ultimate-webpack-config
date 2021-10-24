a7d5daeeffeb18652ddfc994818305bec179b7bf
Szymon Wiszczuk <golota60@gmail.com>
2021-10-24 20:54:31 +0200

    add linaria

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './styles/styles.css';
 const recipe = {
   ham: 3,
   cheese: 2
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,44 @@
 
 /** @type {import('webpack').Configuration} */
 const mode = process.env.NODE_ENV || "development";
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: mode,
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader"
-      },
-    }]
-  },
+  plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' })],
   devtool: "source-map",
   devServer: {
     static: "./dist"
+  },
+  
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: process.env.NODE_ENV !== 'production',
+          },
+        },
+      ],
+    },{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [
+      {
+        loader: "babel-loader",
+      },    
+      {
+        loader: '@linaria/webpack-loader',
+        options: {
+          sourceMap: process.env.NODE_ENV !== 'production',
+        },
+      }
+    ]
+    }]
   },
 }
